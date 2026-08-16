4bd257f23c6f432e9a94a59229b7c3849e689363
DomRadomski <domradomski123@gmail.com>
2026-08-16 15:25:05 +0100

    catController v1

---
+++ controllers/categoryController.js
@@ -0,0 +1,53 @@
+const { body, validationResult, matchedData } = require("express-validator");
+const db = require("../db/queries/categoryQueries");
+
+async function catListGet(req, res) {
+    const categories = await db.getAllCats();
+    res.render("categories", {categories});
+}
+
+async function catItemGet(req, res) {
+    const category = await db.getCatItem(req.params.id);
+    res.render("category", {category});
+}
+
+async function catCreateGet(req, res) {
+    res.render("createCategory");
+}
+
+async function catItemPost(req, res) {
+    const { name, description } = req.body;
+    await db.createCat(name, description);
+    const category = await db.getLatestCat();
+    res.render("category", {category});
+}
+
+async function catUpdateGet(req, res) {
+    const category = await db.getCatItem(req.params.id);
+    res.render("updateCategory", {category})
+}
+
+async function catUpdatePost(req, res) {
+    const id = req.params.id;
+    const { name, description } = req.body;
+    await db.updateCat(id, name, description);
+    const category = await db.getCatItem(id);
+    res.render("category", {category});
+}
+
+async function catDeleteGet(req, res) {
+    const category = await db.getCatItem(req.params.id);
+    res.render("deleteCategory", {category})
+}
+
+async function catDeletePost(req, res) {
+    await db.deleteCatItem(req.params.id)
+    const categories = await db.getAllCats();
+    res.render("categories", {categories});
+}
+
+
+
+
+
+
