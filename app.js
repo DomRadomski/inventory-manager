30466096fa74049bf369dd0a16a46a8a6e5e0899
DomRadomski <domradomski123@gmail.com>
2026-08-22 22:03:31 +0100

    view: homepage

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.send("Welcome to the homepage my driller")
+  res.render("index");
 });
 
 app.use("/categories", catRouter);
